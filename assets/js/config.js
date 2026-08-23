097801ed20db48ee584c0b60876b671a27fbfa84
alphawebsolutions <alphawebsolutionsindia@gmail.com>
2026-08-23 15:01:51 +0530

    Update config.js
    api

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,7 +3,7 @@
   /exec URL below. Leave it blank to use the bundled demo content.
 */
 window.ALPHA_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxzN5FNT6JGe90842SPUuujpGkyoP6lJY5GxXvFA8a0-RMfPfjrIU7E5Ur9w0lEhpk/exec",
   alphaWebsite: "https://alphawebsolutions.github.io/",
   alphaWhatsApp: "919392417891",
   alphaEmail: "alphawebsolutionsindia@gmail.com"
